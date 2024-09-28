a5cf8c2ce45d995943637d315fa38e0b54014860
Timotei Dumitru <dumitru.timy@gmail.com>
2024-09-28 19:40:16 +0100

    update undefined result of objectCall file

--- javascript_algorithms/interviews/objectCall.js
+++ javascript_algorithms/interviews/objectCall.js
@@ -2,11 +2,14 @@ class User {
   constructor(firstName) {
     this.firstName = firstName;
   }
-  static getName() {
+  // static getName() { // cus is a static method, that's why return undefined
+  getName() { // this will return desired result
     return this.firstName;
   }
 }
 
 const newUser = new User("Jessie");
 
-console.log(User.getName()); // will return undefined
+// console.log(User.getName()); // will return undefined -> cus is calling User class instead of newUser constant
+console.log(newUser.getName()); // this will return the desired result
+
